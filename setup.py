b769369458731105a51f82fdb2637b9acfe0fd30
jwpetley <jwpetley@gmail.com>
2021-12-20 14:37:40 +0000

    Initial Commit for Composite Spectra Package

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='CoSpecPy',
+      version='0.1',
+      # list folders, not files
+      packages=find_packages(),
+      )
