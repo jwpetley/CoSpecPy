afef04b991e82667f3075191f6d7549dcc3fd555
jwpetley <jwpetley@gmail.com>
2021-12-20 14:54:05 +0000

    Class Init

--- setup.py
+++ setup.py
@@ -6,4 +6,6 @@ setup(name='CoSpecPy',
       version='0.1',
       # list folders, not files
       packages=find_packages(),
+      url="https://github.com/jwpetley/CoSpecPy",
+      python_requires=">=3.6",
       )
