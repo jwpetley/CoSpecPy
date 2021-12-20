3f42bb8f645cf7ffa460103ef210b9fa4bf8b49a
jwpetley <jwpetley@gmail.com>
2021-12-20 15:05:15 +0000

    Updated Setup and Twine Build

--- setup.py
+++ setup.py
@@ -2,10 +2,21 @@
 
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(name='CoSpecPy',
       version='0.1',
+      author = "James Petle"
+      author_email = "jwpetley@gmail.com"
+
+      short_description = "Python Package for SDSS Composite Spectra"
+      long_description = long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/jwpetley/CoSpecPy",
       # list folders, not files
       packages=find_packages(),
-      url="https://github.com/jwpetley/CoSpecPy",
+
+
       python_requires=">=3.6",
       )
