730a9419d0b5f02495fc472dd54f97229cc6a060
jwpetley <jwpetley@gmail.com>
2021-12-20 17:41:59 +0000

    Version 0.3 for PyPi

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(name='CoSpecPy',
-      version='0.2',
+      version='0.3',
       author = "James Petley",
       author_email = "jwpetley@gmail.com",
 
