64d3eeae737b187def6a2ec3855d73d09027e5d7
jwpetley <jwpetley@gmail.com>
2021-12-21 17:18:09 +0000

    Added requirements.txt

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -53,7 +53,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'classic'
+html_theme = 'sphinx_rtd_theme'
 
 pygments_style = 'sphinx'
 
