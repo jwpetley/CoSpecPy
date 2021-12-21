4c6c2e5790704488468a1d8c7bf411b2417cf464
jwpetley <jwpetley@gmail.com>
2021-12-21 13:35:20 +0000

    Restructure of docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,12 +30,8 @@ release = '0.4'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-    'sphinx.ext.duration',
-     'sphinx.ext.autodoc',
-     #"myst_parser",
+extensions = ["myst_parser"
 ]
-source_suffix = ['.rst', '.md']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -43,7 +39,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = []
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -51,7 +47,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'classic'
+html_theme = 'alabaster'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -1,5 +1,5 @@
 .. CoSpecPy documentation master file, created by
-   sphinx-quickstart on Tue Dec 21 12:24:52 2021.
+   sphinx-quickstart on Tue Dec 21 13:33:12 2021.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
@@ -10,7 +10,10 @@ Welcome to CoSpecPy's documentation!
    :maxdepth: 2
    :caption: Contents:
 
+.. toctree::
    getting_started.md
+
+
 
 Indices and tables
 ==================
