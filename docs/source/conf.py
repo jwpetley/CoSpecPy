a231c781e1e46d320db772efd183eb18ca4c74c4
jwpetley <jwpetley@gmail.com>
2021-12-21 17:32:39 +0000

    Added matplotlib requirement

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,6 +12,7 @@
 #
 import os
 import sys
+import sphinx_rtd_theme
 sys.path.insert(0, os.path.abspath('../..'))
 
 
@@ -34,7 +35,7 @@ extensions = ['sphinx.ext.autodoc',
         'sphinx.ext.napoleon',
 ]
 
-autodoc_mock_imports = ["numpy", "scipy", "astropy"]
+autodoc_mock_imports = ["numpy", "scipy", "astropy", "matplotlib"]
 
 
 
